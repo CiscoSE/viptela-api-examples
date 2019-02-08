4da59794f7761e09fac5e7c6fbd7d79ec93b551c
Ed Jackson <edjacks@cisco.com>
2019-02-08 09:28:04 -0500

    updated prompt and output

--- get_unassigned_certificates.py
+++ get_unassigned_certificates.py
@@ -14,22 +14,44 @@ PARAMETERS:
 import vmanageapi
 import json
 import sys
+import getpass
+
+def get_credentials():
+    username = input('Please enter a vManage username: ')
+    password = getpass.getpass(prompt='Please enter the password for ' + username + ': ')
+
+    return(username, password)
+
 
 def main(args):
-    if not len(args) == 3:
-        print(__doc__)
-        return
 
-    vmanage_ip, username, password = args[0], args[1], args[2]
+    if len(args) == 3:
+        vmanage_ip, username, password = args[0], args[1], args[2]
+    elif len(args) == 1:
+        vmanage_ip = args[0]
+        username, password = get_credentials()
+    else:
+        vmanage_ip = input('Please enter vManage IP address: ')
+        username, password = get_credentials()
+
+
     api_call = 'certificate/vedge/list'
 
     response = vmanageapi.main([vmanage_ip, username, password, api_call])
 
     d = json.loads(response)
 
-    for x in range(len(d['data'])):
-        if d['data'][x]['vedgeCertificateState'] == 'tokengenerated':
-            print(x, d['data'][x]['serialNumber'], d['data'][x]['chasisNumber'])
+    if len(d['data']) > 0:
+        print('{: >2}: {: <35} {: <35}'.format('#', 'Token', 'Chassis #'))
+
+        for x in range(len(d['data'])):
+            if d['data'][x]['vedgeCertificateState'] == 'tokengenerated':
+                print('{: >2}: {: <35} {: <35}'.format(x, d['data'][x]['serialNumber'], d['data'][x]['chasisNumber']))
+
+    else:
+        print('No available certificates found')
+
+    print()
 
 
 if __name__ == '__main__':
